feee69ff14c3e043bf85ce94af9adaac1fc7642a
linshunm <linshunm@126.com>
2018-01-31 01:26:13 +0800

    add hook clipboard service

---
+++ app/src/main/java/com/iutils/hook/BinderHookHandler.java
@@ -0,0 +1,55 @@
+package com.iutils.hook;
+
+import android.annotation.TargetApi;
+import android.content.ClipData;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @author weishu
+ * @date 16/2/16
+ */
+public class BinderHookHandler implements InvocationHandler {
+
+    private static final String TAG = "BinderHookHandler";
+
+    // 原始的Service对象 (IInterface)
+    Object base;
+
+
+
+    Class<?> stub;
+
+    public BinderHookHandler(IBinder base) {
+        try {
+            this.stub = Class.forName("android.content.IClipboard$Stub");
+            Method asInterfaceMethod = stub.getDeclaredMethod("asInterface", IBinder.class);
+            // IClipboard.Stub.asInterface(base);
+            this.base = asInterfaceMethod.invoke(null, base);
+        } catch (Exception e) {
+            throw new RuntimeException("hooked failed!");
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        // 把剪切版的内容替换为 "you are hooked"
+        if ("getPrimaryClip".equals(method.getName())) {
+            Log.d(TAG, "hook getPrimaryClip");
+            return ClipData.newPlainText(null, "you are hooked");
+        }
+
+        // 欺骗系统,使之认为剪切版上一直有内容
+        if ("hasPrimaryClip".equals(method.getName())) {
+            return true;
+        }
+
+        return method.invoke(base, args);
+    }
+}
